f1f06849d03fef68996b74c0a0c2cc528f8841ec
Ankit Kumar <ankitkumar820772@gmail.com>
2026-08-29 15:28:58 +0530

    feat: add Remove Duplicates from Sorted Array solution

--- algorithms/remove-duplicates-from-sorted-array/solution.py
+++ algorithms/remove-duplicates-from-sorted-array/solution.py
@@ -1,44 +1,13 @@
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
-        res = []
-        n = len(nums)
-
-        for i in range(n - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-
-            for j in range(i + 1, n - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-
-                left = j + 1
-                right = n - 1
-
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-
-                    if total < target:
-                        left += 1
-
-                    elif total > target:
-                        right -= 1
-
-                    else:
-                        res.append([
-                            nums[i],
-                            nums[j],
-                            nums[left],
-                            nums[right]
-                        ])
-
-                        left += 1
-                        right -= 1
-
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-
-                        while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-
-        return res
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        write_index = 1 
+        
+        for read_index in range(1, len(nums)):
+            if nums[read_index] != nums[read_index - 1]:
+                nums[write_index] = nums[read_index]
+                write_index += 1
+                
+        return write_index
