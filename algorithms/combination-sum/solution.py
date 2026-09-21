72f855d270c2502d7158b3b8327f745a67bb52b8
Ankit Kumar <ankitkumar820772@gmail.com>
2026-09-21 16:53:41 +0530

    feat: add Combination Sum solution

--- algorithms/combination-sum/solution.py
+++ algorithms/combination-sum/solution.py
@@ -1,10 +1,21 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        n=len(needle)
-        i=0
-        while i+n <= len(haystack):
-            if needle == haystack[i:i+n]:
-                return i
-            i+=1
-        return -1
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        results=[]
+        def backtrack(index,current,remaining):
+            if remaining==0:
+                results.append(current.copy())
+                return
+            if remaining<0:
+                return
+            if index == len(candidates):
+                return
+            current.append(candidates[index])
+            backtrack(index,current,remaining-candidates[index])
+            current.pop()
+            backtrack(index+1,current,remaining)
+        backtrack(0,[],target)
+
+        return results
+
+        
         
