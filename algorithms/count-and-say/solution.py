72f7811ca1bfff9561c273dc7c5718151bf65918
Ankit Kumar <ankitkumar820772@gmail.com>
2026-09-25 15:57:19 +0530

    feat: add Count and Say solution

--- algorithms/count-and-say/solution.py
+++ algorithms/count-and-say/solution.py
@@ -1,17 +1,23 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        mydict={}
-        for i in range(len(nums)):
-            need=target-nums[i]
-            if mydict.get(need) != None:
-                return (i,mydict.get(need))
-            else:
-                mydict[nums[i]]=i
-        return []
-        
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def newbuild(rle):
+            res=[]
+            prev=rle[0]
+            count=0
+            for i in rle:
+                if prev== i:
+                    count+=1
+                else:
+                    res.extend([str(count),prev])
+                    prev=i
+                    count=1
+            res.extend([str(count),prev])
+            return "".join(res)
+        final="1"
+        for i in range(n-1):
+            final=newbuild(final)
+        return final
+
+
+
         
